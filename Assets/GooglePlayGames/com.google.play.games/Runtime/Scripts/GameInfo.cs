db54e7d573373feb0e993d750b4ae70de2bed87a
Rio Susanto <riosst100@gmail.com>
2023-03-23 12:57:31 +0700

    Add login scene

--- Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
+++ Assets/GooglePlayGames/com.google.play.games/Runtime/Scripts/GameInfo.cs
@@ -36,9 +36,9 @@ namespace GooglePlayGames {
         private const string UnescapedWebClientId = "WEB_CLIENTID";
         private const string UnescapedNearbyServiceId = "NEARBY_SERVICE_ID";
 
-        public const string ApplicationId = ""; // Filled in automatically
+        public const string ApplicationId = "237393369917"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "237393369917-v6gsekc881l1bpf6d9fflurmpu3ld5e4.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
